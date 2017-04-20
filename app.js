eb854e13bda1dd2c8b44dc577f1dc7c79cbfa06c
lvbiao <lvb_8231853@qq.com>
2017-04-20 17:07:10 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
+var fs= require('fs');
+var uuid = require('node-uuid');
+var multer = require ('multer');
+
+
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/cover');
+  },
+  filename: function (req, file, callback) {
+    //file.originalname   coffee.jpg
+    var fileFormat = (file.originalname).split(".");
+
+    callback(null, uuid.v4() + '.' + fileFormat[fileFormat.length - 1]);
+  }
+});
+var upload = multer({ storage : storage}).single('cover');
+
+var mongoose = require('mongoose');
 var app = express();
 
 var ueditor = require("ueditor");
@@ -12,7 +31,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 // view engine setup
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', ejs.__express);
@@ -20,16 +38,16 @@ app.set('view engine', 'html');
 
 app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
     //客户端上传文件设置
-    var imgDir = '/img/ueditor/'
-     var ActionType = req.query.action;
+    var imgDir = '/img/ueditor/';
+    var ActionType = req.query.action;
     if (ActionType === 'uploadimage' || ActionType === 'uploadfile' || ActionType === 'uploadvideo') {
-        var file_url = imgDir;//默认图片上传地址
+        var file_url = imgDir;//图片上传地址
         /*其他上传格式的地址*/
         if (ActionType === 'uploadfile') {
-            file_url = '/file/ueditor/'; //附件
+            file_url = 'document/file/ueditor/'; //附件
         }
         if (ActionType === 'uploadvideo') {
-            file_url = '/video/ueditor/'; //视频
+            file_url = 'document/video/ueditor/'; //视频
         }
         res.ue_up(file_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
         res.setHeader('Content-Type', 'text/html');
@@ -48,7 +66,52 @@ app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function (req, re
 }));
 
 
+//增加换行
+function appendLine(content,append){
+  return content+append+'\r\n';
+}
 
+function makeContent(content){
+  let result = appendLine('<html>','');
+  result= appendLine(result,'<head>');
+  result=appendLine(result,'<meta name = "viewport" content = "width=device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable = 0" />');
+  result=appendLine(result,'<meta http-equiv="Content-Type" content="text/html; charset=utf-8">');
+  result=appendLine(result,'</head>');
+  result=appendLine(result,'<body>');
+  result=appendLine(result,content);
+  result=appendLine(result,'</body>');
+  result+='</html>';
+  return result;
+}
+
+//以下为路由
+
+app.post('/submit',function(req,res){
+  //let dir = path.join(__dirname, 'public');
+  let folder = uuid.v4();
+  let dir = path.join(__dirname, 'public','document',folder);
+  if (fs.existsSync(dir)) {
+     console.log('已经创建过此目录了');
+   } else {
+     fs.mkdirSync(dir);
+   };
+
+   let content = makeContent(req.body.content);
+   let fileName = uuid.v4();
+   fs.writeFile(path.join(dir, fileName + '.html'), content, 'utf8', (err) => {
+   if (err) throw err;
+ });
+});
+
+app.post('/cover',function(req,res){
+  upload(req,res,function(err) {
+        if(err) {
+          console.log(err);
+          res.json({result:'err'});
+        }
+        res.json({result:'ok',fileName:req.file.filename});
+    });
+});
 
 app.use('/', function (req, res) {
     res.render('ueditor');

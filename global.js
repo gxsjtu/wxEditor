140a20dec0dc1d69500fda030e5520d99f167f5b
郭轩 <guoxuan@guoxuan.local>
2017-05-24 09:45:11 +0800

    OK

--- global.js
+++ global.js
@@ -1,3 +1,5 @@
-module.exports.appId = "wxfa41e36dbf1c347f";
-module.exports.appSecret = "56f87e5085200685087e805f2ae361e7";
+// module.exports.appId = "wxfa41e36dbf1c347f";
+// module.exports.appSecret = "56f87e5085200685087e805f2ae361e7";
+module.exports.appId = "wxe3be88b3096ec6fd";
+module.exports.appSecret = "993e2d28d1c1085c65fbb8d6c2c898d4";
 module.exports.server = "http://painting.shtx.com.cn";
